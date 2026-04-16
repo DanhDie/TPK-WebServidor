1444c34213d697d973ebeddc7e9c819bb0ceaf89
Daniel Dwulatka <138732225+DanhDie@users.noreply.github.com>
2026-04-16 18:49:56 -0300

    Campanha FInalizada

--- Views/viewCampanha.php
+++ Views/viewCampanha.php
@@ -1,4 +1,4 @@
- <section class="container">
+<section class="container">
     <div class="container">
         
         <div class="is-primary">
@@ -7,13 +7,13 @@
             </h1>
 
             <div class="mb-3 mx-2">
-                <nav class="breadcrumb has-bullet-separator" aria-label="breadcrumbs">
+                <nav class="breadcrumb has-bullet-separator">
                     <ul>
                         <li class="is-size-7">
                             <a href="../Controllers/controllerTelaInicial.php">Campanhas</a>
                         </li>
                         <li class="is-active is-size-7 has-text-primary">
-                            <a href="#" aria-current="page">
+                            <a href="#">
                                 <?= $campanhaSelecionada['nomeCampanha'] ?>
                             </a>
                         </li>
@@ -28,10 +28,7 @@
                 <div class="columns">
                     <div class="column is-4">
                         <figure class="image is-4by3">
-                            <img
-                                src="<?= $campanhaSelecionada['imagemCampanha'] ?>"
-                                alt="Imagem da campanha"
-                            />
+                            <img src="<?= $campanhaSelecionada['imagemCampanha'] ?>">
                         </figure>
                     </div>
 
@@ -55,12 +52,12 @@
                 <hr>
 
                 <div class="content columns is-multiline">
-                    <?php if (!empty($campanhaSelecionada['sessoesCampanha'])): 
-                            foreach ($campanhaSelecionada['sessoesCampanha'] as $sessao): ?>
+                    <?php if (!empty($campanhaSelecionada['sessoesCampanha'])): ?>
+                        <?php foreach ($campanhaSelecionada['sessoesCampanha'] as $sessao): ?>
                             <div class="column is-3">
                                 <?php include("../Views/Templates/cardSession.php"); ?>
                             </div>
-                        <?php endforeach;?>
+                        <?php endforeach; ?>
                     <?php endif; ?>
                 </div>
 
@@ -75,30 +72,47 @@
                 <hr>
 
                 <div class="content">
-                    <div class="mt-1 columns is-multiline">
-                        <div class="column is-3">
-                            <?php include("../Views/Templates/cardPersonagem.php"); ?>
+                    <?php if (!empty($campanhaSelecionada['personagensCampanha'])): ?>
+                        <div class="columns is-multiline">
+                            <?php foreach($campanhaSelecionada['personagensCampanha'] as $personagem): ?>
+                                <div class="column is-3">
+                                    <?php include("../Views/Templates/cardPersonagem.php"); ?>
+                                </div>
+                            <?php endforeach; ?>
                         </div>
-                        <div class="column is-3">
-                            <?php include("../Views/Templates/cardPersonagem.php"); ?>
-                        </div>
-                        <div class="column is-3">
-                            <?php include("../Views/Templates/cardPersonagem.php"); ?>
-                        </div>
-                    </div>
+                    <?php else: ?>
+                        <p class="has-text-grey">Nenhum personagem nesta campanha.</p>
+                    <?php endif; ?>
+                </div>
 
-                    <div class="content">
-                        <label class="label has-text-primary">Adicionar personagem:</label>
+                <div class="content">
+                    <label class="label has-text-primary">Adicionar personagem:</label>
 
-                        <div class="select">
-                            <select>
-                                <option>Select dropdown</option>
-                                <option>With options</option>
-                            </select>
+                    <form method="POST" action="../Controllers/controllerCampanha.php">
+                        <input type="hidden" name="idC" value="<?= $campanhaSelecionada['idCampanha'] ?>">
+
+                        <div class="my-2 field has-addons">
+                            <div class="pt-2">
+                                <div class="select is-primary">
+                                    <select name="idP" required>
+                                        <option disabled selected>Selecione um personagem</option>
+
+                                        <?php foreach($usuario['personagens'] as $personagem): ?>
+                                            <option value="<?= $personagem['idPersonagem'] ?>">
+                                                <?= $personagem['nome'] ?>
+                                            </option>
+                                        <?php endforeach; ?>
+
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="px-2 pt-2 is-primary">
+                                <button class="button has-text-primary" type="submit" name="addPersonagem">
+                                    Adicionar
+                                </button>
+                            </div>
                         </div>
-
-                        <button class="button">+</button>
-                    </div>
+                    </form>
                 </div>
 
             </div>

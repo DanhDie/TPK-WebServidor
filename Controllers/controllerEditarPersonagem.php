2b366cc0cc0857bed2d3129e694ee1e03fd0a9dc
Gabriel Covalski <gab.covalski@gmail.com>
2026-04-16 19:57:01 -0300

    EditarPersonagem funcional

--- Controllers/controllerEditarPersonagem.php
+++ Controllers/controllerEditarPersonagem.php
@@ -27,47 +27,6 @@ if ($personagemIndex === null) {
 $personagemSelecionado = $usuario['personagens'][$personagemIndex];
 
 
-include("./validationCriarPersonagem.php");
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['submit']) && $_POST['submit'] === "Atualizar") {
-
-        $usuario['personagens'][$personagemIndex] = [
-            ...$usuario['personagens'][$personagemIndex],
-
-            'nome' => $_POST['nome'],
-            'level' => $_POST['level'],
-            'classe' => $_POST['classe'],
-            'subclasse' => $_POST['subclasse'],
-            'forca' => $_POST['forca'],
-            'destreza' => $_POST['destreza'],
-            'constituicao' => $_POST['constituicao'],
-            'inteligencia' => $_POST['inteligencia'],
-            'sabedoria' => $_POST['sabedoria'],
-            'carisma' => $_POST['carisma'],
-            'vida' => $_POST['vida'],
-            'armadura' => $_POST['armadura'],
-            'velocidade' => $_POST['velocidade'],
-            'historia' => $_POST['historia']
-        ];
-
-        $_SESSION['infoUser'] = $usuario;
-
-        header("Location: controllerFichaPersonagem.php?idP=$idP");
-        exit;
-    }
-
-    if (isset($_POST['excluir'])) {
-
-        unset($usuario['personagens'][$personagemIndex]);
-        $usuario['personagens'] = array_values($usuario['personagens']);
-
-        $_SESSION['infoUser'] = $usuario;
-
-        header("Location: controllerPersonagens.php");
-        exit;
-    }
-}
-
+include("./validationEditarPersonagem.php");
 include("../Views/viewEditarPersonagem.php");
 include("../Views/Templates/footer.php");

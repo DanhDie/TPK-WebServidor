b439f002189a8f481cebb17aa9a6169b2ffe7e3c
Gabriel Covalski <gab.covalski@gmail.com>
2026-05-12 22:14:30 -0300

    MVC + OO

---
+++ Models/modelPersonagem.php
@@ -0,0 +1,18 @@
+<?php 
+    class Personagem{
+        protected $id;
+        protected $nome;
+        protected $classe;
+        protected $subclasse;
+        protected $historia;
+        protected $level;
+        protected $vida;
+        protected $armadura;
+        protected $forca;
+        protected $destreza;
+        protected $constituicao;
+        protected $inteligencia;
+        protected $sabedoria;
+        protected $carisma;
+        protected $velocidade;
+    }
---
+++ Models/modelSessao.php
@@ -0,0 +1,7 @@
+<?php
+    class Sessao{
+        protected $id;
+        protected $nome;
+        protected $data; // Vai ser usado alguma biblioteca para tratar isso aqui?
+        protected $resumo;
+    }

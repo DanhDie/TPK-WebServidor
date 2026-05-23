f9b42651655382ba5d98d5b3c81bd15ef97c72aa
Daniel Dwulatka <138732225+DanhDie@users.noreply.github.com>
2026-05-23 14:16:24 -0300

    Pequeno erro no css

--- resources/Views/viewLogin.php
+++ resources/Views/viewLogin.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/public/CSS/style.css">
+    <link rel="stylesheet" href="../public/CSS/style.css">
     <title>TPK - Login</title>
 </head>
 <body class="">

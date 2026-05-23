03b4693dbafd742cdffa89350632aa45ac238b3e
gabs <gab.covalski@gmail.com>
2026-05-22 22:13:59 -0300

    estruturacao das pasta

--- Models/modelSessao.php
+++
@@ -1,7 +0,0 @@
-<?php
-    class Sessao{
-        protected $id;
-        protected $nome;
-        protected $data; // Vai ser usado alguma biblioteca para tratar isso aqui?
-        protected $resumo;
-    }
